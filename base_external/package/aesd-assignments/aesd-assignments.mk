7beebbc35fdf6d95e026f4a65008ca76c8ce548f
Frederik Barløse <frba@enabl.dk>
2025-05-27 08:47:59 +0200

    added writer path

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '8af0026c768c127e45494e5114ff661775cfd4f4'
+AESD_ASSIGNMENTS_VERSION = '759c87ba4ebce03ffe985a35a8796e3fdafb7846'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
