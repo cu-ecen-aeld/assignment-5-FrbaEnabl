99260c33fe44d5c8f0b0ef6c4ce8f66b00747783
Frederik Barløse <frba@enabl.dk>
2025-06-04 10:10:22 +0200

    Updated hash

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '0e9b2eb5f50cedd7e242b789e361d5e0e0e84fc4'
+AESD_ASSIGNMENTS_VERSION = 'd67476fb6809801ab1d97feb07b3defa28e02a98'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
